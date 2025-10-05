088683502dfd2651a77d256cd8fa46b0bd75fc21
harshvardhan_singh <harshvardhansingh41664@gmail.com>
2025-10-05 23:46:34 +0530

    Project4 Completed

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@tmp
+M=0
+@i
+M=0
+
+(LOOP)
+@i
+D=M
+@R0
+D=D-M
+@MOVE
+D;JEQ
+@R1
+D=M
+@tmp
+M=M+D
+@i
+M=M+1
+@LOOP
+0;JMP
+
+(MOVE)
+@tmp
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
